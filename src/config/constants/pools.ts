41f219f7c7feab7b76e7464676735d46834c0857
Salamiswap <79930809+Salamiswap@users.noreply.github.com>
2021-03-10 18:27:09 +0100

    Update pools.ts

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -2,35 +2,18 @@ import { PoolConfig, QuoteToken, PoolCategory } from './types'
 
 const pools: PoolConfig[] = [
   {
-    sousId: 0,
-    tokenName: 'CAKE',
-    stakingTokenName: QuoteToken.CAKE,
-    stakingTokenAddress: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
-    contractAddress: {
-      97: '0xd3af5fe61dbaf8f73149bfcfa9fb653ff096029a',
-      56: '0x73feaa1eE314F8c655E354234017bE2193C9E24E',
-    },
-    poolCategory: PoolCategory.CORE,
-    projectLink: 'http://pancakeswap.finance/',
-    harvest: true,
-    tokenPerBlock: '10',
-    sortOrder: 1,
-    isFinished: false,
-    tokenDecimals: 18,
-  },
-  {
     sousId: 37,
     tokenName: 'SLM',
     stakingTokenName: QuoteToken.CAKE,
-    stakingTokenAddress: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
+    stakingTokenAddress: '0x78A68Bb21B6fE1385eaaEf233f07C9d433508128',
     contractAddress: {
       97: '',
       56: '0x577b7ef698af98a990c47be44ba5b1eaa49412f3',
     },
     poolCategory: PoolCategory.CORE,
-    projectLink: 'https://frontier.xyz/',
+    projectLink: 'https://salamiswap.xyz/',
     harvest: true,
-    tokenPerBlock: '0.2546',
+    tokenPerBlock: '5',
     sortOrder: 999,
     isFinished: false,
     tokenDecimals: 18,
