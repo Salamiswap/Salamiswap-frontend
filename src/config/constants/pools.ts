21cf7e641f04c1c121642877c6d63977c940df3f
Salamiswap <79930809+Salamiswap@users.noreply.github.com>
2021-03-11 18:36:11 +0100

    Update pools.ts

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -8,7 +8,7 @@ const pools: PoolConfig[] = [
     stakingTokenAddress: '0x22E1fCaACaFa3c1E6e2C1FFB0021E2AC55142Af8',
     contractAddress: {
       97: '',
-      56: '0x577b7ef698af98a990c47be44ba5b1eaa49412f3',
+      56: '0xb87cdf3d5c0984963ac9ab5c08d9225bcb5495bb',
     },
     poolCategory: PoolCategory.CORE,
     projectLink: 'https://salamiswap.xyz/',
